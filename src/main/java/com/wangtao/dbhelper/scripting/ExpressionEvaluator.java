d584c12f80cc1676a2395d472fed4f109a868382
wangtao <wangtao970503@gmail.com>
2019-02-23 23:24:43 +0800

    增加evaluateIterable方法.

--- src/main/java/com/wangtao/dbhelper/scripting/ExpressionEvaluator.java
+++ src/main/java/com/wangtao/dbhelper/scripting/ExpressionEvaluator.java
@@ -1,6 +1,12 @@
 package com.wangtao.dbhelper.scripting;
 
+import com.wangtao.dbhelper.builder.BuilderException;
+
+import java.lang.reflect.Array;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author wangtao
@@ -27,5 +33,35 @@ public class ExpressionEvaluator {
         } else {
             return value != null;
         }
+    }
+
+    /**
+     * 获取表达式的值.
+     * @param root       根对象
+     * @param expression Ognl表达式
+     * @return Iterable对象
+     */
+    public static Iterable<?> evaluateIterable(Object root, String expression) {
+        Object value = OgnlCache.getValue(root, expression);
+        if (value == null) {
+            throw new BuilderException("The value of expression '" + expression +
+                    "' equals null. We need a iterable object!");
+        }
+        if (value instanceof Iterable<?>) {
+            return (Iterable<?>) value;
+        }
+        if (value.getClass().isArray()) {
+            List<Object> results = new ArrayList<>();
+            int length = Array.getLength(value);
+            for (int i = 0; i < length; i++) {
+                results.add(Array.get(value, i));
+            }
+            return results;
+        }
+        if (value instanceof Map<?, ?>) {
+            return ((Map<?, ?>) value).entrySet();
+        }
+        throw new BuilderException("Error evaluate the expression '" + expression + "'."
+                + "The value '" + value + "' was not iterable.");
     }
 }
